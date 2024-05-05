7b8a2ddbeb4686bfa36acbc5740f257d4ad85aa2
bogdan9876 <bogdankryveiko@gmail.com>
2024-05-05 23:42:08 +0300

    change mc

--- Patient_Health_Monitoring_Code/Patient_Health_Monitoring_Code.ino
+++ Patient_Health_Monitoring_Code/Patient_Health_Monitoring_Code.ino
@@ -1,87 +1,67 @@
-
 #include <TimerOne.h>
-
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+LiquidCrystal lcd(17, 16, 6, 7, 8, 9);
 
 int val;
-int tempPin = A0;// temperature Sensor Pin
-int HBSensor = 7;// Sensor Pin
+int tempPin = 36;
+int HBSensor = 14;
 int HBCount = 0;
 int HBCheck = 0;
 int TimeinSec = 0;
 int HBperMin = 0;
-int HBStart = 2;
+int HBStart = 31;
 int HBStartCheck = 0;
 
-void setup() 
-{
-  // put your setup code here, to run once:
+void setup() {
   lcd.begin(20, 4);
   pinMode(HBSensor, INPUT);
   pinMode(HBStart, INPUT_PULLUP);
-  Timer1.initialize(800000); 
+  Timer1.initialize(50000); 
   Timer1.attachInterrupt( timerIsr );
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("HEALTH MONITORING");
   lcd.setCursor(0,1);
   lcd.print("TIME IN SEC : ");
-  lcd.setCursor(0,3);
-  lcd.print("BODY TEMP   : ");
   lcd.setCursor(0,2);
   lcd.print("HB PER MIN  : ");
-  
 }
 
-void loop() 
-{
-  val = analogRead(tempPin);
-  float mv = (val/1023.0)*5000;
-  int cel = mv/10;
-  lcd.setCursor(14,3);
-  lcd.print(cel);
-  lcd.print("   ");
-  delay(100);
-  
-  if(digitalRead(HBStart) == LOW)
-  {
+void loop() {
+
+  if(digitalRead(HBStart) == LOW) {
     HBStartCheck = 1;
   }
   
-  if(HBStartCheck == 1)
-  {
-    if((digitalRead(HBSensor) == HIGH) && (HBCheck == 0))
-      {
-        HBCount = HBCount + 1;
-        HBCheck = 1;
-      }
-      
-      if((digitalRead(HBSensor) == LOW) && (HBCheck == 1))
-      {
-        HBCheck = 0;   
-      }
-      
-      if(TimeinSec == 10)
-      {
-          HBperMin = HBCount * 6;;
-          HBStartCheck = 0;
-          lcd.setCursor(14,2);
-          lcd.print(HBperMin);
-          lcd.print(" ");
-          HBCount = 0;
-          TimeinSec = 0;      
-      }
+  if(HBStartCheck == 1) {
+    if((digitalRead(HBSensor) == HIGH) && (HBCheck == 0)) {
+      HBCount = HBCount + 1;
+      HBCheck = 1;
+    }
+    
+    if((digitalRead(HBSensor) == LOW) && (HBCheck == 1)) {
+      HBCheck = 0;   
+    }
+    
+    if(TimeinSec == 10) {
+      HBperMin = HBCount * 6;;
+      HBStartCheck = 0;
+      lcd.setCursor(14,2);
+      lcd.print(HBperMin);
+      lcd.print(" ");
+      HBCount = 0;
+      TimeinSec = 0;      
+    }
   }
+  
 }
 
-void timerIsr()
-{
-  if(HBStartCheck == 1)
-  {
-      TimeinSec = TimeinSec + 1;
-      lcd.setCursor(14,1);
-      lcd.print(TimeinSec);
-      lcd.print(" ");
+void timerIsr() {
+  if(HBStartCheck == 1) {
+    TimeinSec = TimeinSec + 1;
+    lcd.setCursor(14,1);
+    lcd.print(TimeinSec);
+    lcd.print(" ");
   }
 }
